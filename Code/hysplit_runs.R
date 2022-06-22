7c34a0a7398c78812817f15cec43c919a4eb0c97
Justin Fain <jstnfain@gmail.com>
2022-06-22 10:50:17 -0400

    Update hysplit_runs to include support for cached met files and model height max.

--- Code/hysplit_runs.R
+++ Code/hysplit_runs.R
@@ -5,8 +5,12 @@ require(lubridate)
 require(here)
 #
 
+# Check for met and exec directories. Create them if they aren't already there.
+# if(!dir.exists(here('met'))){system('mkdir met')}
+# if(!dir.exists(here('exec'))){system('mkdir exec')}
+
 # Begin model ----
-# Single dispersion model run. Change default assumptions/time steps here
+# Single dispersion model run. Change default assumptions/time steps here.
 dispersion_run_alias <- function(latitude, longitude, date_time,
                                  model_period_s = 3600,
                                  release_offset_s = 600,
@@ -14,12 +18,13 @@ dispersion_run_alias <- function(latitude, longitude, date_time,
                                  rate = 500,
                                  pdiam = 0.25,
                                  dsty = 1,
-                                 shape_factor = 1){
+                                 shape_factor = 1,
+                                 hmax = 1000) {
 
-  # Theese are the only *required* arguments to be provided via API
+  # These are the only *required* arguments to be provided via API
   burn <- data.frame(latitude, longitude, burn_dt = date_time)
 
-  #' NOTE: The date_time field must be provided in a cannonical format that
+  #' NOTE: The date_time field must be provided in a canonical format that
   #' can be ingested by lubridate functions such as ymd_hms. I've set
   #' truncated=3 to handle incomplete dates for most cases.
 
@@ -35,14 +40,18 @@ dispersion_run_alias <- function(latitude, longitude, date_time,
       density = dsty,
       shape_factor = shape_factor,
       release_start = ymd_hms(burn$burn_dt, truncated = 3) + release_offset_s,
-      release_end = ymd_hms(burn$burn_dt, truncated = 3) + release_offset_s + model_period_s,
+      release_end = ymd_hms(burn$burn_dt, truncated = 3) + release_offset_s + model_period_s
     ) %>%
     # Set simulation time and met info
     add_dispersion_params(
       start_time = ymd_hms(burn$burn_dt, truncated = 3),
       end_time = ymd_hms(burn$burn_dt, truncated = 3) + model_period_s,
       direction = "forward",
-      met_type = "reanalysis"
+      met_type = "reanalysis",
+      met_dir = here('met'),
+      exec_dir = here('exec'),
+      clean_up = T,
+      model_height = hmax
     ) %>%
     run_model()
 }
@@ -65,7 +74,7 @@ test_runs[[1]] %>% dispersion_plot()
 test_runs[[2]] %>% dispersion_plot()
 test_runs[[3]] %>% dispersion_plot()
 
-# All plots together to prove alignmnet
+# All plots together to prove alignment
 test_runs %>%
   lapply(. %>%  `$`('disp_df')) %>%
   bind_rows() %>%
